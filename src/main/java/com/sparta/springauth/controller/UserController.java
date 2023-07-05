dede2b854042b9e764276816e3d44e768bb0fe27
NayoungKim1212 <kkd04250@naver.com>
2023-07-05 11:41:01 +0900

    로그인 구현 : JWT

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserService userService;
+
     @GetMapping("/user/login-page")
     public String loginPage() {
         return "login";
@@ -28,5 +31,16 @@ public class UserController {
     public String signup(SignupRequestDto requestDto) {
         userService.signup(requestDto);
         return "redirect:/api/user/login-page";
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error"; // = e.getMessage()
+        }
+
+        return "redirect:/";
     }
 }
